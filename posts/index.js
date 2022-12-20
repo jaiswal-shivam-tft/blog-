e967842fe0655e801b5896d96ea512a0bf2f19db
jaiswalshiva <jaiswal.shivam@tftus.com>
2022-12-20 09:50:30 +0530

    first message

---
+++ posts/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+app.post('/posts', async (req, res) => {
+  //   const id = randomBytes(4).toString('hex');
+  const id = uuidv4();
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
+  res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
+});
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
